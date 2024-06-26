b9486c740ea8c84af3fcf83dd36db984fbf6bec0
seungchan08 <s24041@gsm.hs.kr>
2024-06-26 19:52:12 +0900

    java-basic

---
+++ src/HelloJava2.java
@@ -0,0 +1,8 @@
+public class HelloJava2 {
+
+    public static void main(String[] args) {
+        System.out.println("Helo Java1");
+        System.out.println("Helo Java2");
+        System.out.println("Helo Java3");
+    }
+}
