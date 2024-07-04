e55a2d10b287150701848bdef1067f28a176b28f
seungchan08 <s24041@gsm.hs.kr>
2024-07-04 17:56:31 +0900

    java-basic

--- src/array/Array1Ref2.java
+++ src/array/Array1Ref2.java
@@ -1,6 +1,6 @@
 package array;
 
-public class Array1Ref1 {
+public class Array1Ref2 {
 
     public static void main(String[] args) {
         int[] students;//배열 변수 선언
@@ -14,10 +14,8 @@ public class Array1Ref1 {
         students[4] = 50;
 
        //변수 값 사용
-        System.out.println("학생1 점수:" + students[1]);
-        System.out.println("학생2 점수:" + students[2]);
-        System.out.println("학생3 점수:" + students[3]);
-        System.out.println("학생4 점수:" + students[4]);
-        System.out.println("학생5 점수:" + students[5]);
+        for(int i = 0; i < students.length; i++){
+            System.out.println("학생" + (i + 1) + " 점수: " + students[i]);
+        }
     }
 }
